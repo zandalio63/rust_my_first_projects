2bf2e6bc384980b6925558dc07a64e18894bdf2f
Sebastián Laverde <151546685+sbtn63@users.noreply.github.com>
2026-03-17 20:23:56 -0500

    Delete basic_calculator/target directory

--- basic_calculator/target/debug/basic_calculator.d
+++
@@ -1,1 +0,0 @@
-/root/Rust/Projects/basic_calculator/target/debug/basic_calculator: /root/Rust/Projects/basic_calculator/src/main.rs
--- basic_calculator/target/debug/deps/basic_calculator-b9baf3e8e36bab5d.d
+++
@@ -1,5 +0,0 @@
-/root/Rust/Projects/basic_calculator/target/debug/deps/basic_calculator-b9baf3e8e36bab5d.d: src/main.rs
-
-/root/Rust/Projects/basic_calculator/target/debug/deps/basic_calculator-b9baf3e8e36bab5d: src/main.rs
-
-src/main.rs:
