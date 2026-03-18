d98e9325bb832e032b2a5ed8e10ef362953ce7d2
Sebastián Laverde <151546685+sbtn63@users.noreply.github.com>
2026-03-17 20:24:26 -0500

    Delete mini_bank/target directory

--- mini_bank/target/debug/deps/mini_bank-ec353399c2b8a3d5.d
+++
@@ -1,5 +0,0 @@
-/root/Rust/Projects/mini_bank/target/debug/deps/mini_bank-ec353399c2b8a3d5.d: src/main.rs
-
-/root/Rust/Projects/mini_bank/target/debug/deps/libmini_bank-ec353399c2b8a3d5.rmeta: src/main.rs
-
-src/main.rs:
--- mini_bank/target/debug/mini_bank.d
+++
@@ -1,1 +0,0 @@
-/root/Rust/Projects/mini_bank/target/debug/mini_bank: /root/Rust/Projects/mini_bank/src/main.rs
